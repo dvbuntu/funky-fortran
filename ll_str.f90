2931b9abefec72b8334724bb1039a605666c39ab
Dan Van Boxel <the.dvb@gmail.com>
2018-01-06 13:53:32 +0930

    Adjust LL to hide string type.

--- ll_str.f90
+++ ll_str.f90
@@ -14,16 +14,26 @@ module ll_str
 
 contains
 
+type(string) function make_str(chars)
+    character(len=*), intent(in) :: chars
+    type(string) :: new_val
+    allocate(character(len=len(chars)) :: new_val%str)
+    new_val%str = chars
+    make_str = new_val
+end function make_str
 
 subroutine list_init(self,init_val)
     ! Create a new linked list with the base value
     type(node), pointer :: self
-    type(string), intent(in) :: init_val
+    character(len=*), intent(in) :: init_val
+    type(string) :: new_val
+
+    new_val = make_str(init_val)
 
     allocate(self)
     nullify(self%next)
 
-    self%value = init_val
+    self%value = new_val
     self%initialized = .true.
 
 end subroutine list_init
@@ -32,19 +42,21 @@ subroutine list_push(self, val)
     ! Put a new node on the front
     type(node), pointer :: self
     type(node), pointer :: tmp
-    type(string), intent(in) :: val
+    character(len=*), intent(in) :: val
     type(string), allocatable :: newstring
     integer :: i
+
+
 
     ! Make memory for and copy string
     allocate(newstring)
     
-    newstring%str = val%str(1:len(val%str))
+    newstring%str = val(1:len(val))
 
     if ( .not. associated(self)  ) then
-        call list_init(self, newstring)
+        call list_init(self, newstring%str)
     else if (.not. self%initialized) then
-        call list_init(self, newstring)
+        call list_init(self, newstring%str)
     else
         ! Get new memory
         allocate(tmp)
@@ -60,10 +72,12 @@ subroutine list_push(self, val)
 
 end subroutine list_push
 
-type(string) function list_pop(self)
+!type(string) function list_pop(self)
+function list_pop(self) result(res)
     ! Return top value and set head to next
     type(string) :: val
     type(node), pointer :: self, tmp
+    character(:), allocatable :: res
     ! Grab the value
     val = self%value
     ! Save to dealloc later
@@ -73,7 +87,8 @@ type(string) function list_pop(self)
     ! Free memory
     deallocate(tmp)
 
-    list_pop = val ! This is stupid and confusing
+    !allocate(res(len(val%str)))
+    res = val%str
     return
 end function list_pop
 
@@ -109,7 +124,7 @@ end function list_len
 integer function list_find(self, val)
     ! Find index of value in list
     ! If not found, return -1
-    type(string), intent(in) :: val
+    character(len=*), intent(in) :: val
     integer :: idx, ans
     type(node), target :: self
     type(node), pointer :: tmp
@@ -118,7 +133,7 @@ integer function list_find(self, val)
     idx = 0
 
     find: do while ( associated(tmp))
-        if (tmp%value%str == val%str) then
+        if (tmp%value%str == val) then
             ans = idx  
         end if
         idx = idx + 1
@@ -130,14 +145,14 @@ end function list_find
 
 subroutine list_remove(self, val)
     ! Remove the first occurrence of val from the list
-    type(string), intent(in) :: val
+    character(len=*), intent(in) :: val
     type(node), pointer :: self
     type(node), pointer :: tmp, prev, next
     tmp => self
     nullify(prev)
 
     find: do while ( associated(tmp))
-        if (tmp%value%str == val%str) then
+        if (tmp%value%str == val) then
             next => tmp%next
             ! Do special if first element
             if (.not. associated(self)) then
@@ -177,44 +192,45 @@ end subroutine list_free
 end module ll_str
 
 
-!program linker
-!! Make a simple linked list with pointers
-!    use ll_str
-!    implicit none
-!    type(node), pointer :: head
-!    integer :: ii 
-!    type(string) :: foo, val
-!    type(string) :: strs(0:9)
-!
-!    foo%str = "FOO"
-!
-!    do ii = 0, 9
-!        allocate(character(len=10) :: strs(ii)%str)
-!        write(strs(ii)%str,'(i10)') ii
-!        !strs(ii)%str = char(i+30)
-!        write(*,*) strs(ii)%str
-!        strs(ii)%str = adjustl(strs(ii)%str)
-!    end do
-!
-!    write(*,*) "Linked List"
-!    call list_init(head, strs(0))
-!    do ii = 1,9
-!        call list_push(head, strs(ii))
-!    end do
-!    call list_print(head)
-!    write(*,*) "Len:", list_len(head)
-!    val = list_pop(head)
-!    write(*,*) "Popped:", val%str
-!    call list_print(head)
-!    write(*,*) "Len:", list_len(head)
-!    val%str = 'val'
-!    write(*,*) "Where's 'val'?:", list_find(head,val)
-!    val%str = '3'
-!    write(*,*) "Where's '3'?:", list_find(head,val)
-!    call list_remove(head,val)
-!    call list_print(head)
-!    call list_free(head)
-!    nullify(head)
-!    write(*,*) "Len:", list_len(head)
-!
-!end program linker
+program linker
+! Make a simple linked list with pointers
+    use ll_str
+    implicit none
+    type(node), pointer :: head
+    integer :: ii 
+    character(len=:), allocatable :: foo
+    character(len=:), allocatable :: val
+    type(string) :: strs(0:9)
+
+    foo = "FOO"
+
+    do ii = 0, 9
+        allocate(character(len=10) :: strs(ii)%str)
+        write(strs(ii)%str,'(i10)') ii
+        !strs(ii)%str = char(i+30)
+        write(*,*) strs(ii)%str
+        strs(ii)%str = adjustl(strs(ii)%str)
+    end do
+
+    write(*,*) "Linked List"
+    call list_init(head, strs(0)%str)
+    do ii = 1,9
+        call list_push(head, strs(ii)%str)
+    end do
+    call list_print(head)
+    write(*,*) "Len:", list_len(head)
+    val = list_pop(head)
+    write(*,*) "Popped:", val
+    call list_print(head)
+    write(*,*) "Len:", list_len(head)
+    val = 'val'
+    write(*,*) "Where's 'val'?:", list_find(head,val)
+    val = '3'
+    write(*,*) "Where's '3'?:", list_find(head,val)
+    call list_remove(head,val)
+    call list_print(head)
+    call list_free(head)
+    nullify(head)
+    write(*,*) "Len:", list_len(head)
+
+end program linker
