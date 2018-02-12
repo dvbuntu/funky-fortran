5d45ffa877991fd31c9026ee80f09f6d625647bf
Dan Van Boxel <the.dvb@gmail.com>
2018-02-12 21:35:27 +0930

    Add working integer heap!

--- heap.f90
+++ heap.f90
@@ -31,52 +31,118 @@ subroutine heap_init(self,init_val)
 
 end subroutine heap_init
 
+subroutine heap_pos(self,idx,tmp,pos, bit)
+    ! find the location of the most recently added node
+    ! tmp will hold that node, and pos its parent
+    ! Seems to allocate new nodes...
+    type(node), pointer :: self
+    type(node), pointer, intent(out) :: pos
+    type(node), pointer, intent(out) :: tmp
+    integer :: idx, nsteps, step, bit, lead
+
+    tmp => self
+
+    ! Chase down position and insert new node
+    ! stupidly enough, this modify's self%size itself!
+    ! idx = self%size +1
+    ! So replace all mentions of with self%size +1...dumb
+    lead = leadz(self%size + 1)
+    nsteps = bit_size(self%size + 1) - lead - 1
+
+
+    do step = 1, nsteps
+        !bit = ibits(self%size + 1,lead + step,1)
+        ! Counting starts from first 1...not from left
+        ! And it's zero up...
+        bit = ibits(self%size + 1,nsteps-step,1)
+        if (bit == 0 .and. step < nsteps) then
+            tmp => tmp%left
+        else if (bit == 0 .and. step == nsteps) then
+            ! allocate new node here
+            pos => tmp
+!            allocate(tmp%left)
+!            tmp => tmp%left
+!            pos%left => tmp
+            exit
+        else if (bit == 1 .and. step < nsteps) then
+            tmp => tmp%right
+        else if (bit == 1 .and. step == nsteps) then
+            ! allocate new node here
+            pos => tmp
+!            allocate(tmp%right)
+!            tmp => tmp%right
+!            pos%right => tmp
+            exit
+        else
+            write(*,*) "Shouldn't be here!"
+        end if
+    end do
+
+
+
+end subroutine heap_pos
+
 subroutine heap_add(self, val)
     ! Add to lowest, rightmost position
     type(node), pointer :: self
     type(node), pointer :: pos
     type(node), pointer :: tmp
     integer, intent(in) :: val
-    integer :: idx, nsteps, step, bit, lead
+    integer :: idx, nsteps, step, bit, lead, tmpval
+    logical :: assoc
 
     if ( .not. associated(self)  ) then
         call heap_init(self, val)
     else if (.not. self%initialized) then
         call heap_init(self, val)
     else
-        tmp => self
+!        tmp => self
 
-        ! Chase down position and insert new node
-        idx = self%size + 1
-        lead = leadz(idx)
-        nsteps = bit_size(idx) - lead - 1
+!        ! Chase down position and insert new node
+!        idx = self%size + 1
+!        lead = leadz(idx)
+!        nsteps = bit_size(idx) - lead - 1
+!
+!
+!        do step = 1, nsteps
+!            !bit = ibits(idx,lead + step,1)
+!            ! Counting starts from first 1...not from left
+!            ! And it's zero up...
+!            bit = ibits(idx,nsteps-step,1)
+!            if (bit == 0 .and. associated(tmp%left)) then
+!                tmp => tmp%left
+!            else if (bit == 0 .and. .not. associated(tmp%left)) then
+!                ! allocate new node here
+!                pos => tmp
+!                allocate(tmp%left)
+!                tmp => tmp%left
+!                pos%left => tmp
+!            else if (bit == 1 .and. associated(tmp%right)) then
+!                tmp => tmp%right
+!            else if (bit == 1 .and. .not. associated(tmp%right)) then
+!                ! allocate new node here
+!                pos => tmp
+!                allocate(tmp%right)
+!                tmp => tmp%right
+!                pos%right => tmp
+!            else
+!                write(*,*) "Shouldn't be here!"
+!            end if
+!        end do
 
+        bit = 0
+        call heap_pos(self, self%size, tmp, pos, bit)
 
-        do step = 1, nsteps
-            !bit = ibits(idx,lead + step,1)
-            ! Counting starts from first 1...not from left
-            ! And it's zero up...
-            bit = ibits(idx,nsteps-step,1)
-            if (bit == 0 .and. associated(tmp%left)) then
-                tmp => tmp%left
-            else if (bit == 0 .and. .not. associated(tmp%left)) then
-                ! allocate new node here
-                pos => tmp
-                allocate(tmp%left)
-                tmp => tmp%left
-                pos%left => tmp
-            else if (bit == 1 .and. associated(tmp%right)) then
-                tmp => tmp%right
-            else if (bit == 1 .and. .not. associated(tmp%right)) then
-                ! allocate new node here
-                pos => tmp
-                allocate(tmp%right)
-                tmp => tmp%right
-                pos%right => tmp
-            else
-                write(*,*) "Shouldn't be here!"
-            end if
-        end do
+        ! Allocate new memory and rejigger pointers
+        if (bit == 0) then
+            allocate(tmp%left)
+            tmp => tmp%left
+            pos%left => tmp
+        else if (bit == 1) then
+            allocate(tmp%right)
+            tmp => tmp%right
+            pos%right => tmp
+        end if
 
         ! Assign the value in tmp
         if (associated(pos)) then
@@ -93,14 +159,20 @@ subroutine heap_add(self, val)
             self%size = self%size + 1
         end if
 
+
         ! Now bubble up the value
-        do while(associated(pos) .and. pos%value > tmp%value)
-            ! We already have an integer variable, why not use?
-            idx = tmp%value 
-            tmp%value = pos%value
-            pos%value = idx
-            pos => pos%parent
-            tmp => tmp%parent
+        do while(associated(pos))
+            if (pos%value > tmp%value) then
+                ! We already have an integer variable, why not use?
+                ! HERE HERE HERE, something broken
+                tmpval = tmp%value 
+                tmp%value = pos%value
+                pos%value = tmpval
+                pos => pos%parent
+                tmp => tmp%parent
+            else
+                exit
+            end if
         end do
 
     end if
@@ -109,50 +181,120 @@ end subroutine heap_add
 
 integer function heap_pop(self)
     ! Return root node and update the heap
-    integer :: val
-    type(node), pointer :: self, tmp
+    ! Need to "bubble down" highest val when removing
+    integer :: val, bit, tmpval
+    type(node), pointer :: self, tmp, pos
+
+    ! Check if we're empty
+    if (.not. associated(self)) then
+        write(*,*) "Nothing Left!"
+        heap_pop = -1 ! Maybe should just error...
+        return
+    else if (self%size == 0) then
+        write(*,*) "Nothing Left!"
+        heap_pop = -1 ! Maybe should just error...
+        return
+    else if (self%size == 1) then
+        heap_pop = self%value
+        call heap_free(self)
+        return
+    end if
+
     ! Grab the value
     val = self%value
+
+    ! Replace with most recently added
+    ! And nuke the most recent position, tmp
+    bit = 0
+    ! Could be dangerous
+    self%size = self%size - 1
+    call heap_pos(self, self%size, tmp, pos, bit)
+!    write(*,*) "FOO", bit
+!    call heap_print(pos)
+!    write(*,*) "BAR", pos%value
+!    call heap_print(tmp)
+!    write(*,*) "GIT"
+    ! This seems to swap the values...
+    if (bit == 0) then
+        self%value = tmp%left%value
+        tmp => pos%left
+        pos%left => null()
+    else
+        self%value = tmp%right%value
+        tmp => pos%right
+        pos%right => null()
+    end if
+    
+    deallocate(tmp)
+
     ! Start winding down the tree
     tmp => self
 
     ! Swap parent with smaller child and continue down
+    ! Only swap if smaller than both
+    ! Some problem HERE HERE HERE
     do while(associated(tmp%left) .or. associated(tmp%right))
+        ! Both leaves present, check both, use smaller
         if (associated(tmp%left) .and. associated(tmp%right)) then
-            if (tmp%left%value > tmp%right%value) then
-                tmp%value = tmp%right%value
-                tmp => tmp%right
-                cycle 
-            else if (tmp%left%value < tmp%right%value) then
+            if (tmp%left%value < tmp%right%value .and. tmp%left%value < tmp%value) then
+                tmpval = tmp%value
                 tmp%value = tmp%left%value
                 tmp => tmp%left
+                tmp%value = tmpval
+                cycle
+            else if (tmp%right%value < tmp%value) then
+                tmpval = tmp%value
+                tmp%value = tmp%right%value
+                tmp => tmp%right
+                tmp%value = tmpval
                 cycle 
+            else
+                exit
             end if
+        ! Left leaf only
         else if (associated(tmp%left)) then
+            if (tmp%left%value < tmp%value) then
+                tmpval = tmp%value
                 tmp%value = tmp%left%value
                 tmp => tmp%left
+                tmp%value = tmpval
                 cycle 
+            else
+                exit
+            end if
+        ! Right leaf only
         else if (associated(tmp%right)) then
+            ! We should never be here
+            if (tmp%right%value < tmp%value) then
+                tmpval = tmp%value
                 tmp%value = tmp%right%value
                 tmp => tmp%right
+                tmp%value = tmpval
                 cycle 
+            else
+                exit
+            end if
         end if
     end do
 
     ! Double check we're at a leaf
-    if (associated(tmp%left) .or. associated(tmp%right)) then
-        write(*,*) "Shouldn't have any children!"
-    end if
+!    if (associated(tmp%left) .or. associated(tmp%right)) then
+!        write(*,*) "Shouldn't have any children!"
+!    end if
 
-    ! Fix parent
-    if (tmp%parent%left%value == tmp%value) then
-        tmp%parent%left => null()
-    else if (tmp%parent%right%value == tmp%value) then
-        tmp%parent%right => null()
-    end if
+    ! Fix parent, could be dangerous if equal value
+!    if (tmp%parent%left%value == tmp%value) then
+!        tmp%parent%left => null()
+!    else if (tmp%parent%right%value == tmp%value) then
+!        tmp%parent%right => null()
+!    end if
+
 
     ! Free memory
-    if (associated(tmp)) deallocate(tmp)
+!    if (associated(tmp)) then
+!        deallocate(tmp)
+!        nullify(tmp)
+!    end if
 
 
     heap_pop = val ! This is stupid and confusing
@@ -186,100 +328,17 @@ recursive subroutine heap_print(self, depth)
     end if
 end subroutine heap_print
 
-!!integer function list_len(self)
-!!    ! Compute list length
-!!    integer :: l
-!!    type(node), pointer :: self, tmp
-!!    tmp => self
-!!
-!!    l = 0
-!!
-!!    do while ( associated(tmp))
-!!        l = l + 1
-!!        tmp => tmp%next
-!!    end do
-!!
-!!    list_len = l
-!!end function list_len
-!!
-!recursive function heap_find(self, val) result(ans)
-!    ! Find node containing value
-!    ! If not found, return node with null
-!    integer, intent(in) :: val
-!    type(node), target :: self
-!    type(node), pointer :: tmp, ans
-!    tmp => self
-!    ans => null()
-!
-!    if (associated(tmp)) then
-!        if (tmp%value == val) then
-!            ans => tmp
-!            return
-!        else if (associated(tmp%left) .and. tmp%value > val) then
-!            ans => heap_find(tmp%left, val)
-!            if (associated(ans)) return
-!        else if (associated(tmp%right) .and. tmp%value < val) then
-!            ans => heap_find(tmp%right, val)
-!            if (associated(ans)) return
-!        end if
-!    end if
-!end function heap_find
-!
-!subroutine heap_remove(self, val)
-!    ! Remove the occurrence of val from the heap
-!    integer, intent(in) :: val
-!    type(node), pointer :: self
-!    type(node), pointer :: tmp, loc, parent
-!    tmp => self
-!
-!    loc => heap_find(tmp, val)
-!
-!    if (associated(loc)) then
-!        ! Three options, leaf, 1 child, 2 children
-!        ! Leaf
-!        if ( .not. associated(loc%left) .and. .not. associated(loc%right) ) then
-!            ! Remove link from parent
-!            parent => loc%parent
-!            if ( parent%left%value == val) then
-!                parent%left => null()
-!            else if ( parent%right%value == val ) then
-!                parent%right => null()
-!            end if
-!            ! Free memory
-!            deallocate(loc)
-!        ! 1 child
-!        else if ( associated(loc%left) .neqv. associated(loc%right) )  then
-!            ! Replace current with child
-!            if ( associated(loc%left)) then
-!                loc%value = loc%left%value
-!                deallocate(loc%left)
-!                loc%left => null()
-!            else if ( associated(loc%right)) then
-!                loc%value = loc%right%value
-!                deallocate(loc%right)
-!                loc%right => null()
-!            end if
-!        ! 2 children, chase down first right, then left
-!        else
-!            tmp => loc%right
-!            do while (associated(tmp%left))
-!                tmp => tmp%left
-!            end do
-!            loc%value = tmp%value
-!            tmp%parent%left => null()
-!            deallocate(tmp)
-!        end if
-!    end if
-!end subroutine heap_remove
-!
-!recursive subroutine heap_free(self)
-!    ! Safely demolish this list
-!    type(node), pointer :: self, tmp
-!    
-!    if (associated(self%left)) call heap_free(self%left)
-!    if (associated(self%right)) call heap_free(self%right)
-!    deallocate(self)
-!end subroutine heap_free
+recursive subroutine heap_free(self)
+    ! Safely demolish this heap
+    type(node), pointer :: self, tmp
+    
+    ! Depth-first traversal to nuke
+    if (associated(self)) then
+        if (associated(self%left)) call heap_free(self%left)
+        if (associated(self%right)) call heap_free(self%right)
+        deallocate(self)
+    end if 
+end subroutine heap_free
 
 end module heap
 
@@ -301,12 +360,19 @@ program heaptest
 !    do n = 0, 32
 !        print "(B32,3(' ',I2))", 12, 12, ibits(12,n,1)
 !    end do
+!    do n = 0, 31
+!        print "(B32,3(' ',I2))", 3, 3, ibits(3,n,1), n
+!        !bit = ibits(idx,nsteps-step,1)
+!    end do
 !    ! Ah, the position starts from the right!
 
     write(*,*) "Min Heap"
     call heap_init(head, 1)
+    write(*,*) "SIZE", head%size
     call heap_add(head, 0)
+    write(*,*) "SIZE", head%size
     call heap_add(head, 7)
+    write(*,*) "SIZE", head%size
     call heap_add(head, 4)
     call heap_add(head, 3)
     call heap_add(head, 4)
@@ -315,25 +381,40 @@ program heaptest
     call heap_add(head, 8)
     call heap_add(head, 5)
     call heap_print(head)
+    write(*,*) "SIZE", head%size
     write(*,*) "Pop root"
     write(*,*) heap_pop(head)
+    write(*,*) "SIZE", head%size
     call heap_print(head)
     write(*,*) "Pop root"
     write(*,*) heap_pop(head)
     call heap_print(head)
-!    write(*,*) "Remove 4"
-!    call heap_remove(head,4)
-!    call heap_print(head)
-!!    write(*,*) "Len:", list_len(head)
-!!    val = list_pop(head)
-!!    write(*,*) "Popped:", val
-!!    call list_print(head)
-!!    write(*,*) "Len:", list_len(head)
-!!    write(*,*) "Where's 3?:", list_find(head,3)
-!!    write(*,*) "Where's 22?:", list_find(head,22)
-!    call heap_free(head)
-!    call heap_print(head)
-!!    nullify(head)
+    write(*,*) "Nuke list"
+    call heap_free(head)
+    call heap_print(head)
+
+    nullify(head)
+
+    ! New heap
+    write(*,*) "New list"
+    call heap_init(head, 1)
+    call heap_add(head, 0)
+    call heap_add(head, 7)
+    call heap_add(head, 4)
+    call heap_add(head, 3)
+    call heap_print(head)
+    do i=1,10
+        write(*,*) "Pop root", i
+        if (associated(head)) then
+            write(*,*) "size", head%size
+        end if
+        call heap_print(head)
+        write(*,*) heap_pop(head)
+    end do
+
+    write(*,*) "Nuke list"
+    call heap_free(head)
+    call heap_print(head)
 !!    write(*,*) "Len:", list_len(head)
 
 end program heaptest
